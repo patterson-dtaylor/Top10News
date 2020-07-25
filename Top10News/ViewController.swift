fb3e861766aac8723714db4e3ce18889692cbc77
patterson-dtaylor <patterson.dtaylor@gmail.com>
2020-07-25 06:46:59 -0400

    Initial Commit

---
+++ Top10News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top10News
+//
+//  Created by Taylor Patterson on 7/25/20.
+//  Copyright © 2020 Taylor Patterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
