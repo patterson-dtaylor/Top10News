881d58f51a0ae573193fda3041d2ec74aafa031b
patterson-dtaylor <patterson.dtaylor@gmail.com>
2020-07-27 00:04:18 -0400

    login and registration controller setup

---
+++ Top10News/Utils/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Top10News
+//
+//  Created by Taylor Patterson on 7/25/20.
+//  Copyright © 2020 Taylor Patterson. All rights reserved.
+//
+
+import UIKit
+
+//MARK: - Fonts
+
+let loraBold = "Lora-Bold"
+let ralewayRegular = "Raleway-Regular"
+
+//MARK: - Colors
+
+let grayText = "regularTextGray"
+let blackText = "boldTextBlack"
+let gradientBG = "gradientBackgorund"
